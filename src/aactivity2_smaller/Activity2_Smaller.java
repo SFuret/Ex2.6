ddc5d0c6480acf29a123cf293a11575c03da66b6
Sandra <>
2023-11-23 10:47:17 +0100

    Mayor y menor

---
+++ src/aactivity2_smaller/Activity2_Smaller.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aactivity2_smaller;
+
+import java.util.Scanner;
+
+public class Activity2_Smaller {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("This program can give you the biggest and smallest number of three");
+        System.out.println("Write a number");
+        int variable_1 = scanner.nextInt();
+        
+        System.out.println("Write another number");
+        int variable_2 = scanner.nextInt();
+        
+        
+        System.out.println("Write the third number");
+        int variable_3 = scanner.nextInt();
+        int small;
+        int big;
+        
+      
+        if(variable_1 > variable_2 && variable_1 > variable_3){
+            big = variable_1;
+            
+            if(variable_2 > variable_3){
+            
+                small = variable_3;
+                }else{
+                    
+                small = variable_2;
+                }
+            System.out.println("The biggest number is " + big + " The smallest number is:" + small );            
+        }
+        
+       else if(variable_2 > variable_1 && variable_2 > variable_3){
+            big= variable_2;
+      
+           if(variable_1 > variable_3){
+                    
+            small = variable_3;
+                }else{
+              small = variable_1;
+           }
+            System.out.println("The biggest number is " + big + "The smallest number is:" + small);        
+            
+        }
+        
+        if(variable_3 < variable_1 && variable_3 < variable_2){
+            big= variable_3;
+            if(variable_2 > variable_1){
+           
+            small = variable_1;
+                }else{
+                 small = variable_2;
+                }
+                    
+            System.out.println("The biggest number is " + big + "The smallest number is:" + small);        
+            
+        }    
+  
+    
+}
+}
